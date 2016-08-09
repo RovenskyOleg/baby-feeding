e2ab1cf6cc27118264ef007e28dffe2a61bf0e10
Oleg Rovenskyi <oleg_rovenskyi@epam.com>
2016-08-09 17:09:43 +0300

    add bootstrap alert to about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'about',
   pipes: [],
   providers: [],
-  directives: [],
+  directives: [AlertComponent],
   styleUrls: ['./about.component.css'],
   templateUrl: './about.component.html'
 })
 
 export class AboutComponent {
+  date = new Date();
 }
