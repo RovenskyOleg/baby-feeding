c94ff317e22c6bb47dd434e022119008771afa21
Oleg Rovenskyi <oleg_rovenskyi@epam.com>
2016-08-10 12:49:12 +0300

    add ng2-charts lib and tet chart

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,13 +1,38 @@
 import {Component} from '@angular/core';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 
 @Component({
-  selector: 'report',
-  pipes: [],
-  providers: [],
-  directives: [],
-  styleUrls: ['./report.component.css'],
-  templateUrl: './report.component.html'
+    selector: 'report',
+    pipes: [],
+    providers: [],
+    directives: [CHART_DIRECTIVES],
+    styleUrls: ['./report.component.css'],
+    templateUrl: './report.component.html'
 })
 
 export class ReportComponent {
+    public lineChartData:Array<any> = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
+    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartType:string = 'line';
+    public pieChartType:string = 'pie';
+
+    // Pie
+    public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+    public pieChartData:number[] = [300, 500, 100];
+
+    public randomizeType():void {
+        this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+        this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+    }
+
+    public chartClicked(e:any):void {
+        console.log(e);
+    }
+
+    public chartHovered(e:any):void {
+        console.log(e);
+    }
 }
